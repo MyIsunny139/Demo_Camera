3c2eadfdb1fbd81bb17da01e24198ba06216e858
MyIsunny139 <15729132737@139.com>
2025-12-17 11:22:20 +0800

    加入camara，sdcard组件

--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -554,6 +554,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("E:/soft/ESP-project/esp32-demo-Assembly/Demo_Camera/build/esp-idf/spi-sdcard/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("E:/soft/ESP-project/esp32-demo-Assembly/Demo_Camera/build/esp-idf/espressif__esp_jpeg/cmake_install.cmake")
 endif()
 
--- main/Demo_Camera.c
+++ main/Demo_Camera.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "esp_camera.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include "spi-sdcard.h"
 
 void app_main(void)
 {
-    
+    printf("Hello, Camera!\n");
 }
